b12b9c97f581e4575bef279ba350efa121cd64d1
haleem737 <gmmkk2009@gmail.com>
2017-12-27 06:53:34 +0300

    working on create new order page 2.0

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <!-- bootstrap framework -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
     
-    <!-- online fonts -->
+    <!-- online icons and fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Archivo' rel='stylesheet'>
 
@@ -22,6 +22,15 @@
         body{
             background:#e6e6e6;
         }
+        a{
+            color:black;
+        }
+        
+        a:hover{
+            text-decoration:none;
+            color:black;
+        }
+
     </style>
 
     <!-- custome style -->
@@ -41,9 +50,6 @@
     <!-- jquery framework -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     
-    <!-- jquery ui -->
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
     <!-- jquery typehead plugin -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>
 
--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -25,12 +25,42 @@ margin:60px;
 margin-bottom:30px;
 }
 
+#controls{
+margin-left:60px;
+margin-bottom:10px;
+}
+
+a[name=delete]{
+padding:10px;
+background:lightgray;
+color:black;
+}
+
+a[name=delete]:hover{
+padding:10px;
+background:darkgray;
+color:white;
+}
+
+a[name=add-row]{
+padding:10px;
+background:lightgray;
+color:black;
+}
+
+a[name=add-row]:hover{
+padding:10px;
+background:darkgray;
+color:white;
+}
+
 #table-header{
 background:#8e9999;
 color:white;
 line-height:50px;
 font-size:1.2em;
 font-weight: normal;
+border-bottom: 5px solid #f1f1f1;
 }
 
 </style>
@@ -48,9 +78,9 @@ font-weight: normal;
         </div>
     </div>
 
-    <!-- search client name -->
     <div class='row' id='header'>
 
+        <!-- search client name -->
         <div class='col-md-7 offset-1'>
 
             <div class='col-md-7'>
@@ -70,20 +100,20 @@ font-weight: normal;
     </div>
 
     <!-- data input table controls -->
-    <div class='row'>
-        <div class='col-md-3 offset-7'>
-            <button type="button" name='add-row' class="control fa fa-plus fa-lg">Add Item</button>
-            <button type="button" name='delete' class="control fa fa-minus fa-lg">Delete Item</button>
-        </div>
+    <div class='text-left' id='controls'>
+        <a href='#' name='add-row' class="control fa fa-plus fa-lg btn-md"> ADD</a>
+        <a href='#' name='delete' class="control fa fa-trash fa-lg btn-md"> DEL</a>
     </div>
 
-
+    <!-- table div -->
     <div class='row' style='padding:0 60px'>
 
         <div class='col-md-12'>
 
+            <!-- table -->
             <table id='order-table' class='talbe-striped'>
             
+                    <!-- table header -->
                     <tr id='table-header' align='center'>
                     
                         <th class='col-md-.1' style='font-weight:normal'>#</th>
@@ -99,6 +129,7 @@ font-weight: normal;
                     
                     </tr>
                 
+                    <!--table inputs  -->
                     <tr>
 
                         <td>
@@ -109,15 +140,16 @@ font-weight: normal;
                             </label>
                         </td>
 
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
-                        <td><input type='text' class='col-md-12 form-control form-control-lg'></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+                        <td><input type='text' class='col-md-12 form-control form-control-lg' autocomplete="off" ></td>
+
                     </tr>
                             
             </table>
@@ -125,6 +157,9 @@ font-weight: normal;
         </div>
 
     </div>
+
+    <!-- footer -->
+    <div class="fixed-bottom">fotter here</div>
 
 </div>
 
@@ -134,90 +169,90 @@ font-weight: normal;
 <script>
 $(document).ready(function(){
 
-    // using typehahead plugin to search for client from database
-    $('#client-name').typeahead({
-        highlight: true,
-        source: function(query, result){
-         $.ajax({
-          url:"{{ URL::to('find-client') }}",
-          method:"GET",
-          data:{query:query},
-          dataType:"json",
-          success:function(data)
-          {
-           result($.map(data, function(client){
-            return client;
-           }));
-          }
-         })
+// using typehahead plugin to search for client from database
+$('#client-name').typeahead({
+    highlight: true,
+    source: function(query, result){
+        $.ajax({
+        url:"{{ URL::to('find-client') }}",
+        method:"GET",
+        data:{query:query},
+        dataType:"json",
+        success:function(data)
+        {
+        result($.map(data, function(client){
+        return client;
+        }));
         }
-       });
+        })
+    }
+    });
 
 
+// input data table functions
+// set first inputs row index
+var row_index = 1;
+$('.row-index').html(row_index);
 
-	// set first inputs row index
-	var row_index = 1;
-	$('.row-index').html(row_index);
+// total of input rows
+var total_ipnut_rows = 1;
 
-	// total of input rows
-	var total_ipnut_rows = 1;
+// define action var to apply a function when control button clicked
+var action;
 
-	// define action var to apply a function when control button clicked
-	var action;
+// check the clicked buttion & fire the required function
+$(document).on('click', '.control' ,function() {
 
-	// check the clicked buttion & fire the required function
-	$(document).on('click', '.control' ,function() {
+    action = $(this).attr('name');
 
-		action = $(this).attr('name');
+    // add row buttion is clicked
+    if(action == 'add-row'){
+        
+        row_index++;
 
-		// add row buttion is clicked
-		if(action == 'add-row'){
-			
-			row_index++;
-
-			var addRow =  jQuery(
-
-
-                        '<tr> <td><label class="custom-control custom-checkbox"><input name="item-row" type="checkbox" class="custom-control-input"><span class="custom-control-indicator"></span><span class="custom-control-description row-index">' + row_index + '</span></label></td>' + 
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td>' +
-						'<td><input type="text" class="col-md-12 form-control form-control-lg"></td> </tr>' );
-
-			$('#order-table').append(addRow);
-			
-			// enable delete inpts row button
-			total_ipnut_rows++;
-			$('[name="delete"]').prop('disabled', false);
-
-		}
-
-		// remove row buttion is clicked
-		if(action == 'delete'){
-
-			if(row_index == 1){
-				return false;
-				$('[name="delete"]').prop('disabled', true);
-			}
-				
-			// remove inputs row
-			$('table tr').has('input[name="item-row"]:checked').remove();
+        var addRow =  jQuery(
 
 
-			// reset input rows index
-			$('.row-index').each(function( index ) {
-				$(this).html(index + 1);
-			});
+                    '<tr> <td><label class="custom-control custom-checkbox"><input name="item-row" type="checkbox" class="custom-control-input"><span class="custom-control-indicator"></span><span class="custom-control-description row-index">' + row_index + '</span></label></td>' + 
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg" autocomplete="off" ></td>' +
+                    '<td><input type="text" class="col-md-12 form-control form-control-lg autocomplete="off" "></td> </tr>' );
 
-			row_index = $('.row-index').length;
+        $('#order-table').append(addRow);
+        
+        // enable delete inpts row button
+        total_ipnut_rows++;
+        $('[name="delete"]').prop('disabled', false);
 
-		}
-	});
+    }
+
+    // remove row buttion is clicked
+    if(action == 'delete'){
+
+        if(row_index == 1){
+            return false;
+            $('[name="delete"]').prop('disabled', true);
+        }
+            
+        // remove inputs row
+        $('table tr').has('input[name="item-row"]:checked').remove();
+
+
+        // reset input rows index
+        $('.row-index').each(function( index ) {
+            $(this).html(index + 1);
+        });
+
+        row_index = $('.row-index').length;
+
+    }
+});
 
 
 
